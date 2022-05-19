d91fbdf81c434018efa0dd051cfac91066b0ea8f
cybiz-tva <ishwarsinghpanwar1122@gmail.com>
2022-05-19 16:00:25 +0530

    Source code with unity built
    Final changes till built test

---
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -0,0 +1,120 @@
+using MoralisUnity.Web3Api.Models;
+using Pixelplacement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : Singleton<GameManager>
+{
+    public static string ContractAddress = "0x769d89A5dF3D7682993A268313b18DEf4937AF43";
+    public static string ContractAbi = "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"bool\",\"name\":\"approved\",\"type\":\"bool\"}],\"name\":\"ApprovalForAll\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256[]\",\"name\":\"ids\",\"type\":\"uint256[]\"},{\"indexed\":false,\"internalType\":\"uint256[]\",\"name\":\"values\",\"type\":\"uint256[]\"}],\"name\":\"TransferBatch\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"TransferSingle\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"string\",\"name\":\"value\",\"type\":\"string\"},{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"}],\"name\":\"URI\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"}],\"name\":\"balanceOf\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address[]\",\"name\":\"accounts\",\"type\":\"address[]\"},{\"internalType\":\"uint256[]\",\"name\":\"ids\",\"type\":\"uint256[]\"}],\"name\":\"balanceOfBatch\",\"outputs\":[{\"internalType\":\"uint256[]\",\"name\":\"\",\"type\":\"uint256[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_tokenId\",\"type\":\"uint256\"},{\"internalType\":\"string\",\"name\":\"_tokenUrl\",\"type\":\"string\"},{\"internalType\":\"bytes\",\"name\":\"data\",\"type\":\"bytes\"}],\"name\":\"buyItem\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"}],\"name\":\"isApprovedForAll\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"renounceOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256[]\",\"name\":\"ids\",\"type\":\"uint256[]\"},{\"internalType\":\"uint256[]\",\"name\":\"amounts\",\"type\":\"uint256[]\"},{\"internalType\":\"bytes\",\"name\":\"data\",\"type\":\"bytes\"}],\"name\":\"safeBatchTransferFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"},{\"internalType\":\"bytes\",\"name\":\"data\",\"type\":\"bytes\"}],\"name\":\"safeTransferFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"internalType\":\"bool\",\"name\":\"approved\",\"type\":\"bool\"}],\"name\":\"setApprovalForAll\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes4\",\"name\":\"interfaceId\",\"type\":\"bytes4\"}],\"name\":\"supportsInterface\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"}],\"name\":\"uri\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]";
+    public static ChainList ContractChain = ChainList.mumbai;
+
+    
+    public enum State
+    {
+        Authenticating,
+        Free,
+        Inventory,
+        Transacting,
+    }
+
+    [SerializeField]
+    private State _currentState;
+    
+    private GameInputActions _input;
+    
+    #region UNITY_LIFECYCLE
+
+    private void Awake()
+    {
+        _currentState = State.Authenticating;
+
+        _input = new GameInputActions();
+        _input.General.Enable();
+    }
+
+    private void OnEnable()
+    {
+        MoralisAuthenticator.Authenticated += OnAuthenticationSuccessfulHandler;
+        Inventory.Opened += OnInventoryOpenedHandler;
+        Inventory.Closed += OnInventoryClosedHandler;
+        
+        PurchaseItemManager.PurchaseStarted += OnPurchaseStartedHandler;
+        PurchaseItemManager.PurchaseCompleted += OnPurchaseCompletedHandler;
+        PurchaseItemManager.PurchaseFailed += OnPurchaseFailedHandler;
+        
+        _input.General.Logout.performed += OnLogoutHandler;
+        _input.General.Quit.performed += OnQuitHandler;
+    }
+
+    private void OnDisable()
+    {
+        MoralisAuthenticator.Authenticated -= OnAuthenticationSuccessfulHandler;
+        Inventory.Opened += OnInventoryOpenedHandler;
+        Inventory.Closed -= OnInventoryClosedHandler;
+        
+        PurchaseItemManager.PurchaseStarted -= OnPurchaseStartedHandler;
+        PurchaseItemManager.PurchaseCompleted -= OnPurchaseCompletedHandler;
+        PurchaseItemManager.PurchaseFailed -= OnPurchaseFailedHandler;
+        
+        _input.General.Logout.performed += OnLogoutHandler;
+        _input.General.Quit.performed += OnQuitHandler;
+    }
+
+    #endregion
+
+
+    #region PUBLIC_METHODS
+
+    public State GetCurrentState()
+    {
+        return _currentState;
+    }
+
+    #endregion
+
+
+    #region EVENT_HANDLERS
+
+    private void OnAuthenticationSuccessfulHandler()
+    {
+        _currentState = State.Free;
+    }
+    
+    private void OnInventoryOpenedHandler()
+    {
+        _currentState = State.Inventory;
+    }
+
+    private void OnInventoryClosedHandler()
+    {
+        _currentState = State.Free;
+    }
+    
+    private void OnPurchaseStartedHandler()
+    {
+        _currentState = State.Transacting;
+    }
+
+    private void OnPurchaseCompletedHandler(string purchasedItemId)
+    {
+        _currentState = State.Free;
+    }
+
+    private void OnPurchaseFailedHandler()
+    {
+        _currentState = State.Free;
+    }
+    
+    private void OnLogoutHandler(InputAction.CallbackContext context)
+    {
+        _currentState = State.Authenticating;
+    }
+    
+    private void OnQuitHandler(InputAction.CallbackContext context)
+    {
+        Application.Quit();    
+    }
+
+    #endregion
+}
